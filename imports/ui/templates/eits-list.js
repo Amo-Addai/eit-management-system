ef49a740efc7cf111c3223b42f7dfa8f222b32ab
Faderr J <johmfaderr@gmail.com>
2018-09-16 02:50:51 +0000

    user submit

--- imports/ui/templates/eits-list.js
+++ imports/ui/templates/eits-list.js
@@ -1,1 +1,9 @@
+import { EITs } from '../../api/eits.js';
+
 import './eits-list.html'
+
+Template.eitlist.helpers({
+    eits:()=>{
+      return EITs.find({}, { sort: { lastname: -1 } });
+    },
+});
--- imports/ui/templates/the-eits.js
+++ imports/ui/templates/the-eits.js
@@ -1,4 +1,32 @@
 import { Template } from 'meteor/templating';
+import { EITs} from '../../api/eits.js';
  
 import './the-eits.html';
- 
+
+Template.body.events({
+    'submit .new-eit'(event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+   
+      // Get value from form element
+      const target = event.target;
+      const firstname = target.first.value;
+      const lastname = target.last.value;
+      const gender = target.gender.value;
+      const dob = target.dob.value;
+   
+      // Insert a task into the collection
+      EITs.insert({
+        firstname,
+        lastname,
+        gender,
+        dob,
+      });
+   
+      // Clear form
+      target.first.value = '';
+      target.last.value = '';
+      target.gender.value.checked = false;
+      target.dob.value = '';
+    },
+  });
